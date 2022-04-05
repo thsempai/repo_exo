8300b0c758844f23b8c15b8a244ef92dc79d5327
Thomas Stassin <thomas.stassin@gsmproductions.com>
2022-04-05 11:31:34 +0200

    change characters in create_word function

--- create_word.py
+++ create_word.py
@@ -1,7 +1,7 @@
 from random import choice
 
 def give_word(number):
-    possibilities = ["a", "h", "k", "o", "n", "s", "t"]
+    possibilities = ["a", "b", "c", "d", "n", "s", "t"]
     word = ""
     for n in range(number):
         word = word + choice(possibilities)
